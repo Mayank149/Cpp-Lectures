438ee2a92eae8d9ce5d9b01fee6f360a959523e5
Mayank149 <bansalmayank1414@gmail.com>
2025-10-08 16:21:06 +0530

    first push file

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // For input/output operations
+
+// Using the standard namespace to avoid repeatedly typing std::
+using namespace std; 
+
+int main() {
+    // Your code goes here
+    cout << "Hello, C++!" << endl; // Example output
+
+    return 0; // Indicates successful program execution
+}
