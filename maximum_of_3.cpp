c8fd2458d250ff379e3f11521371553df18b01ce
Mayank149 <bansalmayank1414@gmail.com>
2025-10-08 16:36:17 +0530

    second max added

--- maximum_of_3.cpp
+++ maximum_of_3.cpp
@@ -6,8 +6,11 @@ int main(){
     int c = 30;
 
     int max = (a > b)? ((a>c) ? a : c) : ((b > c)? b : c);
+    int secondmax = (a > b)? ((a>c) ? (b>c ? b : c) : a) : ((b > c)? (c>a ? c : a) : b);
     cout << "Maximum of 3 numbers is: " << max << endl;
+    cout << "Maximum of 3 numbers is: " << secondmax << endl;
     return 0;
 
 
 }
+
