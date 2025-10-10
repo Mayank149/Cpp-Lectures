dd82563e10896ff0db2a830e2a82efc425a54278
Mayank149 <bansalmayank1414@gmail.com>
2025-10-10 11:21:37 +0530

    subarray sum

--- subarray_of_arrays.cpp
+++ subarray_of_arrays.cpp
@@ -5,12 +5,20 @@ int main() {
    
     int arr[3] = {1,2,3};
 
+    // for(int i = 0; i < 3; i++){
+    //     for(int j = i; j < 3; j++){
+    //         for(int k = i; k <= j; k++){
+    //             cout << arr[k] << " ";
+    //         }
+    //         cout << "\n";
+    //     }
+    // }
+
     for(int i = 0; i < 3; i++){
+        int cursum = 0;
         for(int j = i; j < 3; j++){
-            for(int k = i; k <= j; k++){
-                cout << arr[k] << " ";
-            }
-            cout << "\n";
+            cursum += arr[j];
+            cout << cursum <<"\n";
         }
     }
 
