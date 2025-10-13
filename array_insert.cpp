3bd89d83ff06b044d49546767432102b8d565262
Mayank149 <bansalmayank1414@gmail.com>
2025-10-13 11:31:48 +0530

    Zero position changed to any index

--- array_insert.cpp
+++ array_insert.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main(){
     int arr[7] = {1,2,3,4,5,6,7};
     int n = 7;
-    for(int i = n-1; i > 0; i--){
+    int idx;
+    cout << "Enter the index \n";
+    cin >> idx;
+
+    for(int i = n-1; i > idx; i--){
         arr[i] = arr[i-1];
     }
     int newele;
     cout << "Enter Element \n";
     cin >> newele;
-    arr[0] = newele;
+    arr[idx] = newele;
 
     for(int ele : arr){
         cout << ele << " ";
